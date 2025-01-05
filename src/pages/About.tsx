9294e66763922c9a065d0328c9428428ef98d690
Denisse Rossel <denisserosseln@gmail.com>
2025-01-04 21:19:25 -0300

    Subiendo integración de booststrap en las cards de doctores

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import type { Doctor } from '../types/data';
-import Image from 'react-bootstrap/Image';
+import { Button, Card, Container, Row, Col, Image } from 'react-bootstrap';
 function About() {
   const [doctors, setDoctors] = useState<Doctor[]>([]);
 
@@ -23,31 +23,46 @@ function About() {
 
   return (
     <>
-      <div>
-        <h1>Equipo Médico</h1>
-        <p>
+      <Container className='mt-4'>
+        <h1 className='mb-4 text-center'>Equipo Médico</h1>
+        <p className='text-center mb-5'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas velit,
           cumque placeat non ex voluptatum illo vel natus rerum quam voluptate
           aliquid exercitationem iusto veniam architecto obcaecati itaque eos
           eius!
         </p>
-      </div>
-      <div>
+      <Row>
         {doctors.map((doctor) => (
-          <div key={doctor.id}>
-            <h2>{doctor.name}</h2>
-            <p>{doctor.specialty}</p>
-            <p>{doctor.description}</p>
-            <p>{doctor.services.join(', ')}</p>
-            <p>{doctor.years}</p>
-            <Image
+          <Col key={doctor.id} sm={12} md={6} lg={4} className='mb-4'>
+            <Card>
+              <Card.Body>
+              <Card.Title>{doctor.name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+              {doctor.specialty}
+              </Card.Subtitle>
+              <Card.Text>{doctor.description}</Card.Text>
+              <Card.Text>
+                <strong>Servicios: </strong>
+                {doctor.services.join(', ')}
+              </Card.Text>
+              <Card.Text>
+                <strong>Años de experiencia: </strong>
+                {doctor.years}
+              </Card.Text>
+              <Image
               src={`assets/img/doctors/doctor_${doctor.id}.png`}
               alt={`${doctor.name} - ${doctor.specialty}`}
+              fluid
+              className='mb-3'
             />
-          </div>
+            <Button variant='primary'>Ver más</Button>
+              </Card.Body>
+            </Card>
+            </Col>
         ))}
-      </div>
-    </>
+        </Row>
+        </Container>
+        </>
   );
 }
 
